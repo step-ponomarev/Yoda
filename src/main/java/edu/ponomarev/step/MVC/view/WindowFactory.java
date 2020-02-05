6dd3e84260652aa1fe0408f42e06e36acaa47224
Ponomarev Stepan <htconesuper11@gmail.com>
2020-02-05 13:21:44 +0300

    Changes

---
+++ src/main/java/edu/ponomarev/step/MVC/view/WindowFactory.java
@@ -0,0 +1,17 @@
+package edu.ponomarev.step.MVC.view;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class WindowFactory {
+  public void start() throws Exception {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("windowTest.fxml"));
+    Parent parent = loader.load();
+
+    Stage stage = new Stage();
+    stage.setScene(new Scene(parent));
+    stage.show();
+  }
+}
--- src/main/java/edu/ponomarev/step/Main.java
+++ src/main/java/edu/ponomarev/step/Main.java
@@ -1,8 +1,11 @@
 package edu.ponomarev.step;
 
+
 import edu.ponomarev.step.MVC.controller.Controller;
 import edu.ponomarev.step.system.ApplicationConfigure;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.awt.*;
 
 public class Main {
   public static AnnotationConfigApplicationContext context;
